d6b9b53f1dec216ee33776613a3123c8daa6859b
Elmer Gioseppe Alcoreza <150531204+gioalco@users.noreply.github.com>
2025-11-14 02:09:52 +0800

    Rename tables to lowercase in Section_13.sql (more readability)

--- Section_13.sql
+++ Section_13.sql
@@ -1,55 +1,55 @@
 -- 1. CUSTOMERS Table
-CREATE TABLE CUSTOMERS (
-    CUSTOMER_ID NUMBER(10),
-    LAST_NAME VARCHAR2(25),
-    FIRST_NAME VARCHAR2(25),
-    HOME_PHONE VARCHAR2(12),
-    ADDRESS VARCHAR2(100),
-    CITY VARCHAR2(30),
-    STATE VARCHAR2(2),
-    EMAIL VARCHAR2(25),
-    CELL_PHONE VARCHAR2(12)
+CREATE TABLE customers (
+    customer_id NUMBER(10),
+    last_name VARCHAR2(25),
+    first_name VARCHAR2(25),
+    home_phone VARCHAR2(12),
+    address VARCHAR2(100),
+    city VARCHAR2(30),
+    state VARCHAR2(2),
+    email VARCHAR2(25),
+    cell_phone VARCHAR2(12)
 );
 
 -- 2. MOVIES Table
-CREATE TABLE MOVIES (
-    TITLE_ID NUMBER(10),
-    TITLE VARCHAR2(60),
-    DESCRIPTION VARCHAR2(400),
-    RATING VARCHAR2(4),
-    CATEGORY VARCHAR2(20),
-    RELEASE_DATE DATE
+CREATE TABLE movies (
+    title_id NUMBER(10),
+    title VARCHAR2(60),
+    description VARCHAR2(400),
+    rating VARCHAR2(4),
+    category VARCHAR2(20),
+    release_date DATE
 );
 
 -- 3. MEDIA Table
-CREATE TABLE MEDIA (
-    MEDIA_ID NUMBER(10),
-    FORMAT VARCHAR2(3),
-    TITLE_ID NUMBER(10)
+CREATE TABLE media (
+    media_id NUMBER(10),
+    format VARCHAR2(3),
+    title_id NUMBER(10) 
 );
 
 -- 4. RENTAL_HISTORY Table
-CREATE TABLE RENTAL_HISTORY (
-    MEDIA_ID NUMBER(10),
-    RENTAL_DATE DATE DEFAULT SYSDATE,
-    CUSTOMER_ID NUMBER(10),
-    RETURN_DATE DATE
+CREATE TABLE rental_history (
+    media_id NUMBER(10),
+    rental_date DATE DEFAULT SYSDATE,
+    customer_id NUMBER(10),
+    return_date DATE
 );
 
 -- 5. ACTORS 
-CREATE TABLE ACTORS (
-    ACTOR_ID NUMBER(10),
-    STAGE_NAME VARCHAR2(40),
-    FIRST_NAME VARCHAR2(25),
-    LAST_NAME VARCHAR2(25),
-    BIRTH_DATE DATE
+CREATE TABLE actors (
+    actor_id NUMBER(10),
+    stage_name VARCHAR2(40),
+    first_name VARCHAR2(25),
+    last_name VARCHAR2(25),
+    birth_date DATE
 );
 
 -- 6. STAR_BILLINGS Table
-CREATE TABLE STAR_BILLINGS (
-    ACTOR_ID NUMBER(10),
-    TITLE_ID NUMBER(10),
-    COMMENTS VARCHAR2(40)
+CREATE TABLE star_billings (
+    actor_id NUMBER(10),
+    title_id NUMBER(10),
+    comments VARCHAR2(40)
 );
 
 -- "Run a DESC command for each table."
